9a628acb46685347a48288c6f7df1dd23f578016
Naresh Tanniru <ntanniru@codeaurora.org>
2014-03-05 20:51:41 +0530

    Audio: Bear famiy audio HAL bringup changes
    - Deleted mako spefic files which  are added
      as a part of mako branch integration.
    
    - Renamed media_codecs.xml & media_profile.xml
      to allign with  naming convention
    
    Change-Id: I46677f9b41890c445c79b75ee492705f3f669a27

--- msm8916.mk
+++ msm8916.mk
@@ -6,6 +6,15 @@ ifeq ($(TARGET_USES_QCOM_BSP), true)
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 endif #TARGET_USES_QCOM_BSP
 
+
+
+
+# media_profiles and media_codecs xmls for 8916
+ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
+PRODUCT_COPY_FILES += device/qcom/msm8916/media/media_profiles_8916.xml:system/etc/media_profiles.xml \
+                      device/qcom/msm8916/media/media_codecs_8916.xml:system/etc/media_codecs.xml
+endif
+
 $(call inherit-product, device/qcom/common/common.mk)
 
 PRODUCT_NAME := msm8916
