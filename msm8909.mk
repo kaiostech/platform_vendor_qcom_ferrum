e252b630bcfdfae469d370bfe2f05c61bd42a0dc
Venkata Narendra Kumar Gutta <vgutta@codeaurora.org>
2014-12-30 15:10:56 +0530

    msm8909: Add AEC,NS effects for voice communication
    AEC, NS effects are supported in this board. Add these effects
    for voice communication source, so that VoIP apps can use
    these effects.
    
    CRs-Fixed: 761749
    Change-Id: Id782776c442f5d424807dea7898446f393b36d5b

--- msm8909.mk
+++ msm8909.mk
@@ -120,7 +120,8 @@ PRODUCT_PACKAGES += fstab.qcom
 
 PRODUCT_PACKAGES += \
     libqcomvisualizer \
-    libqcompostprocbundle
+    libqcompostprocbundle \
+    libqcomvoiceprocessing
 
 #OEM Services library
 PRODUCT_PACKAGES += oem-services
