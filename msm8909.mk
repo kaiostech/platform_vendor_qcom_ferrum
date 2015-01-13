a9567fa762bcedf2b80297b5d7aa792c283a17ef
Greg Grisco <ggrisco@codeaurora.org>
2015-01-12 20:21:17 -0800

    Remove reference to packages/apps/Nfc/migrate_nfc.txt.
    Change-Id: I13252ce1303bda45c84f7428ac05b72dd27777b0

--- msm8909.mk
+++ msm8909.mk
@@ -74,7 +74,6 @@ endif
 # Commands to migrate prefs from com.android.nfc3 to com.android.nfc
 # NFC access control + feature files + configuration
 PRODUCT_COPY_FILES += \
-        packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt \
         frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
         frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
         frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
